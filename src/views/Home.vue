a13eca7a5cffb09fd4951a1fe8f86453b78d1b99
OlexandrSai <38455192+OlexandrSai@users.noreply.github.com>
2021-09-10 18:05:31 +0300

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="block md:flex w-full">
+    <Decor :isSignedIn='isSignedIn'/>
+    <SideBar :isSignedIn='isSignedIn'  v-on:signIn='signIn' v-on:signOut='signOut'/>
+    <Main v-if="!isSignedIn"/>
+    <MainLoggedIn v-else  :accountName='accountName' :balance='accountBalance'/>
+  </header>
+</template>
+
+<script>
+import SideBar from '@/components/SideBar.vue'
+import Main from '@/components/Main.vue'
+import Decor from '@/components/Decor.vue'
+import MainLoggedIn from '@/components/MainLoggedIn.vue'
+export default {
+    components: {
+    SideBar,
+    Main,
+    Decor,
+    MainLoggedIn
+  },
+}
+</script>
+
+<style>
+
+</style>
