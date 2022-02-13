af2da16ff882c8970d0236ba18b5124f0d356dda
mihail.shevchuk <mihail.s@hubx.capital>
2022-02-13 18:05:23 +0200

    NG | Home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ArtService} from "../../services/art.service";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public artService: ArtService) { }
 
   ngOnInit(): void {
   }
 
+  async signIn() {
+    await this.artService.nearService.handleSignIn();
+  }
 }
