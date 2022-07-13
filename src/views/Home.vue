f073acba996057357e980b90fe3e6c920a8cfc52
OlexandrSai <38455192+OlexandrSai@users.noreply.github.com>
2022-07-13 23:52:32 +0300

    fixed code style for home view components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
     <section class="bg-gray-400 w-full ">
         <div class="w-full h-full linear-40 inner-shadow">
-
             <div class="p-4 md:px-10 md:py-14 lg:px-9 lg:py-8 xl:px-11 xl:py-12 2xl:px-12 2xl:py-14 mx-auto">
-
                 <!-- Card -->
-                <Card/>
-
+                <Card />
 
                 <!-- Footer -->
-               <Footer/>
-
+                <Footer />
             </div>
-
         </div>
     </section>
 </template>
